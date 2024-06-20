4b4113d59bb373167d964a280ed062440f9eeca1
guoqiushi <guoqiushi910@gmail>
2024-06-20 17:01:15 +0800

    first train

--- dataset.py
+++ dataset.py
@@ -13,10 +13,15 @@ import torch.optim as optim
 from torchvision import datasets, transforms, models
 
 class QualityDataset(Dataset):
-    def __init__(self,root,transform=None):
-        self.root = root
+    def __init__(self,mode,transform=None):
+        # self.root = root
         self.transform = transform
-        self.img_list = glob.glob(os.path.join(self.root,'train','*.jpg'))
+        if mode == 'train':
+            self.img_list = glob.glob('D:/dataset/cityscape/*/leftImg8bit/*/*/*.png')+\
+                            glob.glob('D:/dataset/cityscape/*/leftImg8bit_rain/train/*/*.png')+\
+                            glob.glob('D:/dataset/cityscape/*/leftImg8bit_foggy/train/*/*.png')
+        else:
+            self.img_list = glob.glob('D:/dataset/val_quality/*/*.jpg')
 
     def __len__(self):
         return len(self.img_list)
@@ -24,16 +29,21 @@ class QualityDataset(Dataset):
     def __getitem__(self,index):
 
         img_path = self.img_list[index]
-        label = os.path.basename(img_path).split('.')[0]
-
-        if label =='cat':
-            label = 1
-        else:
+        img_name = os.path.basename(img_path).split('.')[0]
+        if 'rain' in img_name or 'foggy' in img_name:
             label = 0
-
-
+        else:
+            label = 1
         img = Image.open(img_path).convert('RGB')
         if self.transform:
             img = self.transform(img)
 
         return img,label
+
+
+
+
+if __name__ == '__main__':
+    valdata = QualityDataset(mode='val')
+    print(valdata.__getitem__(10))
+
--- train.py
+++ train.py
@@ -14,16 +14,29 @@ import torch.nn as nn
 import torch.optim as optim
 from torchvision import datasets, transforms, models
 from dataset import QualityDataset
+warnings.filterwarnings('ignore')
 
+data_transforms = {
+    'train': transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+    'val': transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ]),
+}
 
-
-all_dataset = QualityDataset('D:/dataset/cat-and-dog/', transform=data_transforms['train'])
-
-train_size = int(0.8 * len(all_dataset))
-val_size = len(all_dataset) - train_size
-train_dataset, val_dataset = random_split(all_dataset, [train_size, val_size])
-train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True,num_workers=4)
-val_loader = DataLoader(val_dataset, batch_size=8, shuffle=False,num_workers=2)
+train_dataset = QualityDataset(mode = 'train', transform=data_transforms['train'])
+val_dataset = QualityDataset(mode='val',transform=data_transforms['val'])
+# train_size = int(0.8 * len(all_dataset))
+# val_size = len(all_dataset) - train_size
+# train_dataset, val_dataset = random_split(all_dataset, [train_size, val_size])
+train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True,num_workers=4)
+val_loader = DataLoader(val_dataset, batch_size=16, shuffle=False,num_workers=2)
 device = 'cuda'
 
 model = torchvision.models.resnet18(pretrained=True)
@@ -62,7 +75,6 @@ def train_model(model, criterion, optimizer, num_epochs=25):
                     outputs = model(inputs)
                     _, preds = torch.max(outputs, 1)
                     loss = criterion(outputs, labels)
-
                     if phase == 'train':
                         loss.backward()
                         optimizer.step()
@@ -70,9 +82,25 @@ def train_model(model, criterion, optimizer, num_epochs=25):
                 running_loss += loss.item() * inputs.size(0)
                 running_corrects += torch.sum(preds == labels.data)
 
+
             epoch_loss = running_loss / dataset_sizes[phase]
             epoch_acc = running_corrects.double() / dataset_sizes[phase]
 
             print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
+    torch.save(model.state_dict(), 'models/model_state.pth')
+    print('save models')
 
-        print()
+
+
+if __name__ == '__main__':
+    # train_model(model,criterion,optimizer,num_epochs=10)
+    img_path = 'D:/dataset/cityscape/cityscape/leftImg8bit/test/mainz/mainz_000000_000093_leftImg8bit.png'
+    model = torchvision.models.resnet18(pretrained=False)
+    num_ftrs = model.fc.in_features
+    model.fc = nn.Linear(num_ftrs, 2)
+    model.load_state_dict(torch.load('models/model_state.pth'))
+    img = Image.open(img_path).convert('RGB')
+    transform = data_transforms['val']
+    img = transform(img)
+    img = torch.unsqueeze(img,0)
+    print(model(img))
